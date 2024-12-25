82cf56d58afdc35f8da8d223f50d0a3dfe17a2f8
Abhinav <abhinavt244@gmail.com>
2024-12-25 20:25:52 +0530

    fixed user

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');  // Import bcrypt for hashing passwords
-const User = require('../models/User');
+const User = require('../models/user');
 const router = express.Router();
 
 // Register Route
